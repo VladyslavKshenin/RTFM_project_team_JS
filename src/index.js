2802340e7ba6cdec0bd37aa0421318a2464af630
VladyslavKshenin <legendary.1bk@gmail.com>
2023-11-09 14:15:40 +0200

    add file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/header.js'
+import './js/mobile-window.js'
+import './js/category-sidebar.js'
+import './js/support-sidebar.js'
+import './js/content-home.js'
+import './js/modal-window.js'
---
+++ src/shopping.js
@@ -0,0 +1,4 @@
+import './js/header.js'
+import './js/mobile-window.js'
+import './js/support-sidebar.js'
+import './js/content-shopping.js'
