223d2da6b8e4a8a21d38d3319f09c55b2cae52d5
VladyslavKshenin <134953305+VladyslavKshenin@users.noreply.github.com>
2023-11-11 11:12:42 +0000

    Deploying to gh-pages from @ VladyslavKshenin/RTFM_project_team_JS@8374cb12a30e6a4565a24573b7b80beec0b58d18 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,36 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";const c="https://books-backend.p.goit.global/books",a={list:"category-list",top:"top-books",category:"category?category="};async function n(o,s=""){try{const t=await fetch(`${c}/${o}${s}`),r=await t.json();return console.log(`data retrieving was succesfull: ${t.status} - ${t.statusText}`),r}catch(t){console.log(`An error occurred: ${t.message}`)}}async function l(o){try{const s=await fetch(`${c}/${o}`),t=await s.json();return console.log(`data retrieving was succesfull: ${s.status} - ${s.statusText}`),t}catch(s){console.log(`An error occurred: ${s.message}`)}}const e={API_OPTIONS:a,fetchBookList:n,fetchBookById:l};function i(){return{bookCollectionWrapper:document.querySelector(".book-collection-wrapper")}}const u=i();function k(o){const s=o.books.map(t=>`<li class="book-card">
+import"./assets/modulepreload-polyfill-ec808ebb.js";const i="https://books-backend.p.goit.global/books",u={list:"category-list",top:"top-books",category:"category?category="};async function k(t,e=""){try{const o=await fetch(`${i}/${t}${e}`),s=await o.json();return console.log(`data retrieving was succesfull: ${o.status} - ${o.statusText}`),s}catch(o){console.log(`An error occurred: ${o.message}`)}}async function p(t){try{const e=await fetch(`${i}/${t}`),o=await e.json();return console.log(`data retrieving was succesfull: ${e.status} - ${e.statusText}`),o}catch(e){console.log(`An error occurred: ${e.message}`)}}const c={API_OPTIONS:u,fetchBookList:k,fetchBookById:p},b=document.querySelector(".ctg-list");c.fetchBookList(c.API_OPTIONS.list).then(t=>{const e=t.map(({list_name:o})=>`<li class="ctg-item js-ctg-item" data-ctg-name = ${o}>${o}
+          </li>`).join("");b.insertAdjacentHTML("beforeend",e)});function m(){return{bookCollectionWrapper:document.querySelector(".book-collection-wrapper")}}const f=m();function h(t){const e=t.books.map(o=>`<li class="book-card">
             <a class="book-link" href="">
               <img
-                src="${t.book_image}"
+                src="${o.book_image}"
                 alt="book image"
                 width="100%"
                 loading="lazy"
               />
               <div class="info">
                 <p class="book-title">
-                  ${t.title}
+                  ${o.title}
                 </p>
                 <p class="book-author">
-                  ${t.author}
+                  ${o.author}
                 </p>
               </div>
             </a>
           </li>`).join("");return`<div class="category-block">
-            <h3>${o.list_name}</h3>
+            <h3>${t.list_name}</h3>
             <ul class="category-books">
-            ${s}
+            ${e}
             </ul>
             <button>more</button>
-          </div>`}function g(o){return u.bookCollectionWrapper.insertAdjacentHTML("beforeend",k(o))}const p={insertCategoryBlocks:g};async function b(){const o=await e.fetchBookList(e.API_OPTIONS.top);console.log(o),o.forEach(p.insertCategoryBlocks)}b();
+          </div>`}function y(t){return f.bookCollectionWrapper.insertAdjacentHTML("beforeend",h(t))}const $={insertCategoryBlocks:y};async function l(){const t=await c.fetchBookList(c.API_OPTIONS.top);console.log(t),t.forEach($.insertCategoryBlocks)}l();const L=document.querySelector(".js-ctg-heading"),v=document.querySelector(".ctg-list"),n=document.querySelector(".selected-ctg-name"),r=document.querySelector(".book-collection-wrapper");console.log(n);v.addEventListener("click",B);L.addEventListener("click",T);function T(){r.innerHTML="",l()}function B(t){const e=t.target.childNodes[0].data;c.fetchBookList(c.API_OPTIONS.category,e).then(o=>{n.innerHTML="",r.innerHTML="";const s=o.map(({book_image:g,author:d,title:a})=>`<div class="ctg-top-books">
+
+  <div class="ctg-book-card">
+    <img class = 'ctg-book-image' src="${g}" alt="${a}" />
+    <div class = 'ctg-book-text'>
+    <h3 class="ctg-book-name">${a}</h3>
+    <h4 class="ctg-book-author">${d}</h4>
+    </div>
+  </div>
+</div>
+`).join("");n.insertAdjacentHTML("afterbegin",e),r.insertAdjacentHTML("beforeend",s)})}
 //# sourceMappingURL=commonHelpers.js.map
