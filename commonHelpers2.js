487a7a75680ff25ac98ef5ba8c6838bfcbe82cb6
VladyslavKshenin <134953305+VladyslavKshenin@users.noreply.github.com>
2023-11-12 13:23:48 +0000

    Deploying to gh-pages from @ VladyslavKshenin/RTFM_project_team_JS@1fa96d95d3d3f7b0a91e417bdc508723abfb6cc1 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,6 +1,4 @@
-import"./assets/support-sidebar-d6b95525.js";const a="localCard",t=document.querySelector(".js-shopping-list"),o=l(a);o[0]!==void 0?(t.innerHTML="",t.insertAdjacentHTML("afterbegin",n(o)),document.querySelector(".btnDelet"),t.addEventListener("click",c)):r();t.insertAdjacentHTML("afterbegin",`<svg class="shopping-list-card-icon-trash" width="12px" height="12px">
-<use href="${logo}#icon-del"></use>
-</svg>`);function c(e){if(e.target.nodeName==="BUTTON"||e.target.nodeName==="svg")t.innerHTML="",o.splice(e.target.id,1),localStorage.removeItem(a),d(a,o),o[0]!==void 0?t.insertAdjacentHTML("afterbegin",n(l(a))):r();else return}function n(e){return e.map(({sebel:s},i)=>`<li class="shopping-list-item">
+import"./assets/support-sidebar-d6b95525.js";const l=[{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"}];localStorage.setItem("localCard",JSON.stringify(l));const o="localCard",t=document.querySelector(".js-shopping-list"),a=r(o);a[0]!==void 0?(t.innerHTML="",t.insertAdjacentHTML("afterbegin",n(a)),t.addEventListener("click",c)):d();function c(s){if(s.target.nodeName==="BUTTON"||s.target.nodeName==="svg"||s.target.nodeName==="use")t.innerHTML="",a.splice(s.target.id,1),localStorage.removeItem(o),g(o,a),a[0]!==void 0?t.insertAdjacentHTML("afterbegin",n(r(o))):d();else return}function n(s){return s.map(({sebel:e},i)=>`<li class="shopping-list-item">
       <img class="shopping-list-card-img" src="https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg" alt="book">
       <div class="shopping-list-card-container">
         <h2 class="shopping-list-card-title">I will find you</h2>
@@ -11,7 +9,7 @@ import"./assets/support-sidebar-d6b95525.js";const a="localCard",t=document.quer
           from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when
           David woke
           suddenly to discover Matthew had been murdered while David was asleep just down the hall.
-          ${s}
+          ${e}
         </p>
         <div class="shopping-list-card-bottom-container">
           <p class="shopping-list-card-author">Harlan Coben</p>
@@ -22,16 +20,16 @@ import"./assets/support-sidebar-d6b95525.js";const a="localCard",t=document.quer
             </a>
           </div>
         </div>
-        <div class="shopping-list-card-container-trash">
-          <button class="btn-shop" id="${i}">
-            <svg class="shopping-list-card-icon-trash" width="12px" height="12px">
+        <div>
+          <button class="btn-shop shopping-list-card-container-trash" id="${i}">
+            <svg class="shopping-list-card-icon-trash" width="18px" height="18px">
               <use href="./images/icons.svg#icon-del"></use>
             </svg>
           </button>
         </div>
       </div>
     </li>
-  `).join("")}function r(){t.insertAdjacentHTML("afterend",`
+  `).join("")}function d(){t.insertAdjacentHTML("afterend",`
     <div class="shopping-list-container">
     <p class="shopping-list-text">
       This page is empty, add some books and proceed to order.
@@ -48,5 +46,5 @@ import"./assets/support-sidebar-d6b95525.js";const a="localCard",t=document.quer
       height="198"
     />
   </div>
-    `)}function d(e,s){try{const i=JSON.stringify(s);localStorage.setItem(e,i)}catch(i){console.error("Get state error: ",i.message)}}function l(e){try{const s=localStorage.getItem(e);return s===null?void 0:JSON.parse(s)}catch(s){console.error("Get state error: ",s.message)}}
+    `)}function g(s,e){try{const i=JSON.stringify(e);localStorage.setItem(s,i)}catch(i){console.error("Get state error: ",i.message)}}function r(s){try{const e=localStorage.getItem(s);return e===null?void 0:JSON.parse(e)}catch(e){console.error("Get state error: ",e.message)}}
 //# sourceMappingURL=commonHelpers2.js.map
