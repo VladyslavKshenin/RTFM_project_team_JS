fe3b0831cb6193e4867c43940f32dee31004f620
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-11-17 03:13:04 +0200

    add_local

--- src/js/content-shopping.js
+++ src/js/content-shopping.js
@@ -6,30 +6,13 @@ import amazon from '../images/modal-window/amazon.png';
 import amazon2x from '../images/modal-window/amazon@2x.png';
 import applebooks from '../images/modal-window/applebooks.png';
 import applebooks2x from '../images/modal-window/applebooks@2x.png';
-const objectCard = [
-  {
-    sebel: "sdfsdfsdf",
-    sdfss: "sdfsdfsdf",
-    fdsdfsdfsdf: "sdfsdf",
-  },
-  {
-    sebel: "sdfsdfsdf",
-    sdfss: "sdfsdfsdf",
-    fdsdfsdfsdf: "sdfsdf",
-  },
-  {
-    sebel: "sdfsdfsdf",
-    sdfss: "sdfsdfsdf",
-    fdsdfsdfsdf: "sdfsdf",
-  },
-];
-localStorage.setItem("localCard", JSON.stringify(objectCard));
 
-const KEY_LOCAL_STORAGE = "localCard";//сюди треба ключ з вебсховища(який буде в модалці створений)
+const KEY_LOCAL_STORAGE = "shoppingCard";//сюди треба ключ з вебсховища(який буде в модалці створений)
+const KEY_LOCAL_STORAGE_ID = "shoppingList";
 const shopEl = document.querySelector(".js-shopping-list");
 const shopContainerEl = document.querySelector('.shopping-list-container');
 const readyCard = parseStorage(KEY_LOCAL_STORAGE);
-
+const numberCard = parseStorage(KEY_LOCAL_STORAGE_ID);
 if (readyCard[0] !== undefined) {
   shopContainerEl.innerHTML="";
   shopEl.innerHTML="";
@@ -39,12 +22,17 @@ if (readyCard[0] !== undefined) {
   funcCreateEmpty();
 }
 function deleteCadr(e) {
-  if (e.target.nodeName === "BUTTON" || e.target.nodeName === "svg" || e.target.nodeName === "use" ) {
+  //e.target.nodeName === "BUTTON" || e.target.nodeName === "svg" || e.target.nodeName === "use" 
+  let parent = e.target.closest('.btn-shop');
+  if (parent) {
   shopEl.innerHTML ="";
-  readyCard.splice(e.target.id, 1);
+  readyCard.splice(parent.id, 1);
+  numberCard.splice(parent.id,1);
   localStorage.removeItem(KEY_LOCAL_STORAGE);
+  localStorage.removeItem(KEY_LOCAL_STORAGE_ID);
 
   setStorage(KEY_LOCAL_STORAGE, readyCard);
+  setStorage(KEY_LOCAL_STORAGE_ID,numberCard);
   if (readyCard[0] !== undefined) {
     shopEl.insertAdjacentHTML(
       "afterbegin",
@@ -62,27 +50,20 @@ else{
 
 function funCreateCard(readyCard) {
   return readyCard
-    .map(({ sebel }, index) => {
+    .map(({  book_image,title,author,description,amazonLinks, appleLinks }, index) => {
       return `<li class="shopping-list-item">
-      <img class="shopping-list-card-img" src="https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg" alt="book">
+      <img class="shopping-list-card-img" src="${book_image}" alt="${title}">
       <div class="shopping-list-card-container">
-        <h2 class="shopping-list-card-title">I will find you</h2>
+        <h2 class="shopping-list-card-title">${title}</h2>
         <p class="shopping-list-card-text">Hardcover fiction</p>
-        <p class="shopping-list-card-desc">
-          David Burroughs was once a devoted father to his three-year-old son Matthew, living a dream life just a short
-          drive away
-          from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when
-          David woke
-          suddenly to discover Matthew had been murdered while David was asleep just down the hall.
-          ${sebel}
-        </p>
+        <p class="shopping-list-card-desc">${description}</p>
         <div class="shopping-list-card-bottom-container">
-          <p class="shopping-list-card-author">Harlan Coben</p>
+          <p class="shopping-list-card-author">${author}</p>
           <div class="shopping-list-card-container-img">
-            <a href="" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
+            <a href="${amazonLinks}" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
               <img class="shopping-list-card-icon-amazon"srcset="${amazon} 1x, ${amazon2x} 2x" src="${amazon}" alt="Amazon" loading="lazy">
             </a>
-            <a href="" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
+            <a href="${appleLinks}" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
               <img class="shopping-list-card-icon-book" srcset="${applebooks} 1x, ${applebooks2x} 2x" src="${applebooks}" alt="Apple books" loading="lazy">
             </a>
           </div>
---
+++ src/js/modal-window.js
@@ -0,0 +1,136 @@
+import getRefs from './refs';
+import api from './books-api';
+// import markup from './markup';
+import amazon from '../images/modal-window/amazon.png';
+import amazon2x from '../images/modal-window/amazon@2x.png';
+import applebooks from '../images/modal-window/applebooks.png';
+import applebooks2x from '../images/modal-window/applebooks@2x.png';
+
+const refs = getRefs();
+const modal = document.getElementById('bookModal');
+const modalTitle = document.getElementById('bookModalTitle');
+const modalBody = document.getElementById('bookModalBody');
+const addToShoppingListBtn = document.getElementById('addToShoppingList');
+const modalCloseBtn = document.querySelector('.modal-close');
+// const modalCloseFooterBtn = document.querySelector('.btn-secondary');
+
+let shoppingList = [];
+let shoppingCard = [];
+let firstCard = {};
+function openModal(bookId) {
+  api.fetchBookById(bookId).then(book => {
+    modalTitle.innerText = book.title;
+    firstCard = {
+      book_image: book.book_image,
+      title: book.title,
+      author: book.author,
+      description: book.description,
+      amazonLinks: book.buy_links[0].url,
+      appleLinks: book.buy_links[1].url,
+    };
+    modalTitle.dataset.bookId = bookId; // Додати dataset для збереження bookId
+    modalBody.innerHTML = `<img src="${book.book_image}" alt="${book.title}" class="book-modal-img"/>
+            <div class="book-modal-details">
+                <h2 class="book-modal-title">${book.title}</h2>
+                <h3 class="book-modal-author">${book.author}</h3>
+                <p class="book-modal-desc">${book.description}</p>
+                <ul class="icon-book-modal-list">
+                    <li>
+                        <a href="${book.buy_links[0].url}" rel="noopener noreferrer nofollow" target="_blank">
+                            <img class="icon-book-modal-amazon" srcset="${amazon} 1x, ${amazon2x} 2x" src="${amazon}" alt="Amazon" loading="lazy"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="${book.buy_links[1].url}" rel="noopener noreferrer nofollow" target="_blank">
+                            <img class="icon-book-modal-ibooks" srcset="${applebooks} 1x, ${applebooks2x} 2x" src="${applebooks}" alt="Apple books" loading="lazy"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    `;
+    
+    // Перевірити, чи книга вже є в списку покупок
+    if (shoppingList.includes(bookId)) {
+      addToShoppingListBtn.innerText = 'Remove from the shopping list';
+    } else {
+      addToShoppingListBtn.innerText = 'Add to shopping list';
+    }
+
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden'; // Вимкнути прокручування, коли відкрито вікно
+  });
+}
+
+function closeModal() {
+  modal.classList.remove('show');
+  document.body.style.overflow = ''; // Увімкнути прокручування, коли вікно закрито
+}
+
+function toggleShoppingList(bookId) {
+  console.log('Toggling shopping list for bookId:', bookId);
+
+  if (!bookId) {
+    console.error('Invalid bookId:', bookId);
+    return;
+  }
+
+  const index = shoppingList.indexOf(bookId);
+  if (index === -1) {
+    shoppingList.push(bookId);
+    shoppingCard.push(firstCard);
+  } else {
+    shoppingList.splice(index, 1);
+    shoppingCard.splice(index,1);
+  }
+
+  console.log('Updated shoppingList:', shoppingList);
+
+  localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+  localStorage.setItem('shoppingCard',JSON.stringify(shoppingCard));
+
+  if (index === -1) {
+    addToShoppingListBtn.innerText = 'Remove from the shopping list';
+  } else {
+    addToShoppingListBtn.innerText = 'Add to shopping list';
+  }
+}
+
+addToShoppingListBtn.addEventListener('click', () => {
+  const currentBookId = modalTitle.dataset.bookId; // Отримання bookId з dataset
+  toggleShoppingList(currentBookId);
+});
+
+modal.addEventListener('click', event => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+
+refs.bookCollectionWrapper.addEventListener('click', event => {
+  const bookLink = event.target.closest('.book-link');
+  if (bookLink) {
+    event.preventDefault();
+    const bookId = bookLink.dataset.id;
+
+    // Виклик функції openModal для відображення модального вікна
+    openModal(bookId);
+  }
+});
+
+// Отримати список покупок із локального сховища
+const savedShoppingList = localStorage.getItem('shoppingList');
+const savedShoppingCard = localStorage.getItem('shoppingCard'); 
+if (savedShoppingList) {
+  shoppingList = JSON.parse(savedShoppingList);
+}
+if (savedShoppingCard) {
+  shoppingCard = JSON.parse(savedShoppingCard);
+}
+modalCloseBtn.addEventListener('click', closeModal);
+// modalCloseFooterBtn.addEventListener('click', closeModal);
