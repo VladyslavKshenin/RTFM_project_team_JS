77f8102f2f46f4056c52598980040166bdd4e849
VladyslavKshenin <134953305+VladyslavKshenin@users.noreply.github.com>
2023-11-17 09:47:32 +0000

    Deploying to gh-pages from @ VladyslavKshenin/RTFM_project_team_JS@9f5adc4dfecea7682b0457f90c26c4ff0416f8be 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,50 +1,43 @@
-import{a as i,b as f,c as d,d as g,l as h}from"./assets/applebooks@2x-b02de412.js";const m="/RTFM_project_team_JS/assets/books-desk@1x-1f62c9bc.png",u="/RTFM_project_team_JS/assets/books-desk@2x-306b4264.png",b="/RTFM_project_team_JS/assets/books-mob@1x-93df6442.png",k=[{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"},{sebel:"sdfsdfsdf",sdfss:"sdfsdfsdf",fdsdfsdfsdf:"sdfsdf"}];localStorage.setItem("localCard",JSON.stringify(k));const r="localCard",o=document.querySelector(".js-shopping-list"),n=document.querySelector(".shopping-list-container"),a=p(r);a[0]!==void 0?(n.innerHTML="",o.innerHTML="",o.insertAdjacentHTML("afterbegin",l(a)),o.addEventListener("click",v)):c();function v(s){if(s.target.nodeName==="BUTTON"||s.target.nodeName==="svg"||s.target.nodeName==="use")o.innerHTML="",a.splice(s.target.id,1),localStorage.removeItem(r),y(r,a),a[0]!==void 0?o.insertAdjacentHTML("afterbegin",l(p(r))):c();else return}function l(s){return s.map(({sebel:e},t)=>`<li class="shopping-list-item">
-      <img class="shopping-list-card-img" src="https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg" alt="book">
+import{a as l,b as _,c as p,d as x,l as S}from"./assets/applebooks@2x-b02de412.js";const v="/RTFM_project_team_JS/assets/books-desk@1x-1f62c9bc.png",y="/RTFM_project_team_JS/assets/books-desk@2x-306b4264.png",L="/RTFM_project_team_JS/assets/books-mob@1x-93df6442.png",r="shoppingCard",a="shoppingList",o=document.querySelector(".js-shopping-list"),i=document.querySelector(".shopping-list-container"),n=c(r),d=c(a);n[0]!==void 0?(i.innerHTML="",o.innerHTML="",o.insertAdjacentHTML("afterbegin",h(n)),o.addEventListener("click",T)):m();function T(e){let s=e.target.closest(".btn-shop");if(s)o.innerHTML="",n.splice(s.id,1),d.splice(s.id,1),localStorage.removeItem(r),localStorage.removeItem(a),g(r,n),g(a,d),n[0]!==void 0?o.insertAdjacentHTML("afterbegin",h(c(r))):m();else return}function h(e){return e.map(({book_image:s,title:t,author:f,description:b,amazonLinks:u,appleLinks:k},$)=>`<li class="shopping-list-item">
+      <img class="shopping-list-card-img" src="${s}" alt="${t}">
       <div class="shopping-list-card-container">
-        <h2 class="shopping-list-card-title">I will find you</h2>
+        <h2 class="shopping-list-card-title">${t}</h2>
         <p class="shopping-list-card-text">Hardcover fiction</p>
-        <p class="shopping-list-card-desc">
-          David Burroughs was once a devoted father to his three-year-old son Matthew, living a dream life just a short
-          drive away
-          from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when
-          David woke
-          suddenly to discover Matthew had been murdered while David was asleep just down the hall.
-          ${e}
-        </p>
+        <p class="shopping-list-card-desc">${b}</p>
         <div class="shopping-list-card-bottom-container">
-          <p class="shopping-list-card-author">Harlan Coben</p>
+          <p class="shopping-list-card-author">${f}</p>
           <div class="shopping-list-card-container-img">
-            <a href="" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
-              <img class="shopping-list-card-icon-amazon"srcset="${i} 1x, ${f} 2x" src="${i}" alt="Amazon" loading="lazy">
+            <a href="${u}" class="shopping-amazon-link" rel="noopener noreferrer nofollow" target="_blank">
+              <img class="shopping-list-card-icon-amazon"srcset="${l} 1x, ${_} 2x" src="${l}" alt="Amazon" loading="lazy">
             </a>
-            <a href="" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
-              <img class="shopping-list-card-icon-book" srcset="${d} 1x, ${g} 2x" src="${d}" alt="Apple books" loading="lazy">
+            <a href="${k}" class="shopping-book-link" rel="noopener noreferrer nofollow" target="_blank">
+              <img class="shopping-list-card-icon-book" srcset="${p} 1x, ${x} 2x" src="${p}" alt="Apple books" loading="lazy">
             </a>
           </div>
         </div>
         <div>
-          <button class="btn-shop shopping-list-card-container-trash" id="${t}">
+          <button class="btn-shop shopping-list-card-container-trash" id="${$}">
             <svg class="shopping-list-card-icon-trash" width="18px" height="18px">
-              <use href="${h}#icon-del"></use>
+              <use href="${S}#icon-del"></use>
             </svg>
           </button>
         </div>
       </div>
     </li>
-  `).join("")}function c(){n.innerHTML="",n.insertAdjacentHTML("afterbegin",`
+  `).join("")}function m(){i.innerHTML="",i.insertAdjacentHTML("afterbegin",`
     <p class="shopping-list-text">
       This page is empty, add some books and proceed to order.
     </p>
     <img
       class="shopping-list-image"
       srcset="
-        ${m} 1x,
-        ${u} 2x
+        ${v} 1x,
+        ${y} 2x
       "
-      src="${b}"
+      src="${L}"
       alt="books"
       width="265"
       height="198"
     />
-    `)}function y(s,e){try{const t=JSON.stringify(e);localStorage.setItem(s,t)}catch(t){console.error("Get state error: ",t.message)}}function p(s){try{const e=localStorage.getItem(s);return e===null?void 0:JSON.parse(e)}catch(e){console.error("Get state error: ",e.message)}}
+    `)}function g(e,s){try{const t=JSON.stringify(s);localStorage.setItem(e,t)}catch(t){console.error("Get state error: ",t.message)}}function c(e){try{const s=localStorage.getItem(e);return s===null?void 0:JSON.parse(s)}catch(s){console.error("Get state error: ",s.message)}}
 //# sourceMappingURL=commonHelpers2.js.map
